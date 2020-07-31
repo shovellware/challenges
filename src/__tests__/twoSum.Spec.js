9535a0d2fe5a08421d715d4553f0fbec23ceae89
Dominic <shovelware@protonmail.ch>
2020-07-31 16:47:31 -0700

    fix spacing?

--- src/__tests__/twoSum.Spec.js
+++ src/__tests__/twoSum.Spec.js
@@ -1,21 +1,21 @@
 import twoSum from '../twoSum';
 
 const testCases = [
-    {
-      target: 9,
-			nums: [ 2, 7, 11, 15 ]
-		},
-		{
-			target: 6,
-			nums: [ 3, 2, 3 ]
-		},
+	{
+		target: 9,
+		nums: [2, 7, 11, 15]
+	},
+	{
+		target: 6,
+		nums: [3, 2, 3]
+	},
 	{
 		target: 6,
 		nums: [3, 2, 4]
 	},
 	{
 		target: 12,
-		nums: [ 11, 7, 8, 2, 29, 16, 4, 33, 10 ]
+		nums: [11, 7, 8, 2, 29, 16, 4, 33, 10]
 	}
 ];
 
